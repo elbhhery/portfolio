d8a3281f8d2753c8772ef290801797fd970cbe83
Ali Elbhhery <you@example.com>
2025-10-03 19:45:18 +0300

    up buton

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ let menu = document.querySelector("nav");
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+let upBtn = document.getElementById("toTop");
+
+window.onscroll = function () {
+  if (window.scrollY >= 600) {
+    upBtn.style.display = "block";
+  } else {
+    upBtn.style.display = "none";
+  }
+};
+
+upBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
